153cf5cc9d53112c204901a0a1dafaa9687ab069
Zinour4 <zzzzzzzzzz20182019@gmail.com>
2026-08-02 22:48:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-import sys
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return "Bot is alive!"
+
+
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.daemon = True
+  t.start()
+
+
+keep_alive()import sys
 import asyncio
 import traceback
 import json
